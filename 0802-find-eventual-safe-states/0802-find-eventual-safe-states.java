431ea3ed64bb2f545b162ad53828f20f77057e8e
Abhijay Dhar <113200707+AbhijayRyan01@users.noreply.github.com>
2026-07-31 15:32:39 +0530

    Time: 28 ms (22.5%), Space: 58 MB (98.67%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<ArrayList<Integer>> adj = new ArrayList<>();
+        int V = graph.length;
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+
+        int[] inDegree = new int[V];
+
+        for(int u = 0; u < V; u++){
+            inDegree[u] = graph[u].length;   
+
+            for(int v : graph[u]){
+                adj.get(v).add(u);           
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safeNodes = new LinkedList<>();
+
+        for(int i=0; i<V; i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            safeNodes.add(node);
+            for(int it: adj.get(node)){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        Collections.sort(safeNodes);
+        
+        return safeNodes;
+    }
+}
